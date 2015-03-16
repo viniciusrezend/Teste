e3f0547fe596a836e1efc151a06a24aeb21711de
vinicius.rezende <vns_rezende@hotmail.com>
2015-03-16 11:01:21 -0300

    Implementação do caso de uso Manter Disciplina

--- Trabalho/src/main/java/br/unicesumar/restserver/disciplina/Disciplina.java
+++ Trabalho/src/main/java/br/unicesumar/restserver/disciplina/Disciplina.java
@@ -1,10 +1,85 @@
 package br.unicesumar.restserver.disciplina;
 
-import java.util.Objects;
+import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@Entity
+public class Disciplina implements Serializable {
+    @Id
+    private Long id;
+    private String nome;
+    private Long cargaHoraria;
+    private Integer peso;
 
-public class Disciplina {
+    public Disciplina() {
+    }
 
+    public Disciplina(Long id, String nome, Long cargaHoraria, Integer peso) {
+        this.id = id;
+        this.nome = nome;
+        this.cargaHoraria = cargaHoraria;
+        this.peso = peso;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Long getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public Integer getPeso() {
+        return peso;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCargaHoraria(Long cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public void setPeso(Integer peso) {
+        this.peso = peso;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 23 * hash + (this.id != null ? this.id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Disciplina other = (Disciplina) obj;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Disciplina{" + "id=" + id + ", nome=" + nome + ", cargaHoraria=" + cargaHoraria + ", peso=" + peso + '}';
+    }
+    
+    
+    
+    
+    
+    
 }
--- Trabalho/src/main/java/br/unicesumar/restserver/disciplina/DisciplinaController.java
+++ Trabalho/src/main/java/br/unicesumar/restserver/disciplina/DisciplinaController.java
@@ -1,6 +1,50 @@
 package br.unicesumar.restserver.disciplina;
 
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(value = "/disciplinas")
+@RestController
+@Transactional
 public class DisciplinaController {
+    
+    @Autowired
+    private EntityManager em;
+    
+    @RequestMapping(method = RequestMethod.POST)
+    public void criarDisciplina(@RequestBody Disciplina disciplina){
+        em.persist(disciplina);
+    }
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Disciplina> getDisciplinas(){
+        Query consulta = em.createQuery("from Disciplina");
+        return consulta.getResultList();
+    }
+    
+    @RequestMapping(method = RequestMethod.PUT)
+    public void editarDisciplina(@RequestBody Disciplina disciplina){
+        disciplina = em.merge(disciplina);
+        em.persist(disciplina);
+    }
+    
+    @RequestMapping(value="/{id}",method = RequestMethod.DELETE)
+    public void excluirDisciplina(@PathVariable Long id){
+        Disciplina disciplina = em.find(Disciplina.class, id);
+        em.remove(disciplina);
+                
+                
+                
+    }
+    
     
 }
