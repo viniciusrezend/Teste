edfb20112f3117869da50cc63a121f570ee3b35f
vinicius.rezende <vns_rezende@hotmail.com>
2015-03-17 09:03:43 -0300

    Concluida as alterações na classe disciplina.js

--- Trabalho/src/main/webapp/pages/disciplinas/disciplina.js
+++ Trabalho/src/main/webapp/pages/disciplinas/disciplina.js
@@ -1,3 +1,70 @@
 module = angular.module("Trabalho", []);
 
 module.controller("DisciplinaController", ["$scope", "$http", DisciplinaController]);
+
+$scope.iniciar = funcaoIniciar;
+$scope.salvar = funcaoSalvar;
+$scope.editar = funcaoEditar;
+$scope.excluir = funcaoExcluir;
+
+$scope.disciplina = {};
+$scope.disciplinas = [];
+$scope.isNovo = true;
+
+function funcaoIniciar(){
+    console.log = ("Iniciando");
+    funcaoCarregar();
+    console.log("Disciplinas carregadas");
+}
+
+function funcaoCarregar(){
+    $http.get("/disciplinas").success(onSuccess).error(onError);
+    
+    function onSuccess(data,status){
+        $scope.disciplinas = data;
+        console.log(data);
+    }
+    function onError(data,status){
+        alert("Deu erro");
+        
+    }
+}
+
+function funcaoSalvar(){
+    if($scope.isNovo){
+        $http.post("/disciplinas", $scope.disciplina).success(onSuccess).error(onError);    
+    }else{
+        $http.put("/disciplinas", $scope.disciplina).success(onSuccess).error(onError);
+    }
+    
+    function onSuccess(data,status){
+        console.log(data);
+        funcaoCarregar();
+        $scope.disciplina = {};
+        $scope.isNovo = true;
+    }
+    
+    function onError(data,status){
+        alert("Deu erro");
+    }
+    
+}
+
+function funcaoEditar(vitima){
+    disciplina = angular.copy(disciplina);
+    $scope.isNovo = false;
+}
+
+function funcaoExcluir(vitima){
+    $http.delete("/disciplinas" + vitima.id).success(onSuccess).error(onError);
+    
+    function onSuccess(data,status){
+        console.log(data);
+        funcaoCarregar();
+        
+    }
+    
+    function onError(data,status){
+        alert("Deu erro");
+    }
+}
